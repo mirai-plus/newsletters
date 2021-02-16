a7298d80a469e5cfc6c84b345ca92847852ff3df
Francesco Luppi <francescoluppi@aol.com>
2021-02-16 18:20:59 +0100

    ADD: changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
-const config = require("./config");
+
 const postMethods = require("./posts");
+const config = require("./config");
+const addHomePage = require("./homepage");
 
 const posts = fs
   .readdirSync(config.dev.postsdir)
-  .map(post => post.slice(0, -5))
+  .map(post => post.slice(0, -3))
   .map(post => postMethods.createPost(post))
-console.log(posts);
+  .sort(function(a, b) {
+    return b.attributes.date - a.attributes.date;
+  });
+
+if (!fs.existsSync(config.dev.outdir)) fs.mkdirSync(config.dev.outdir);
+
+postMethods.createPosts(posts);
+addHomePage(posts);
