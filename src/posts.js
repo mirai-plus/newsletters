08474a10ae79c23796f1d6c91163514a1b7f2914
Francesco Luppi <francescoluppi@aol.com>
2021-02-16 19:34:54 +0100

    FIX: rimosso il blocco di codice di markup

--- src/posts.js
+++ src/posts.js
@@ -1,14 +1,12 @@
 const config = require("./config");
 const fm = require("front-matter");
 const fs = require("fs");
-const marked = require("./marked");
 
 const posthtml = data => `${data.body}`;
 
 const createPost = postPath => {
   const data = fs.readFileSync(`${config.dev.postsdir}/${postPath}.md`, "utf8");
   const content = fm(data);
-  content.body = marked(content.body);
   content.path = postPath;
   return content;
 };
