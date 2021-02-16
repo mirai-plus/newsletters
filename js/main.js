d4e5ce68030c8d33271bc9383cb02c7f3e4c9109
Francesco Luppi <francescoluppi@aol.com>
2021-02-16 15:51:02 +0100

    ADD: tailwind e altri moduli, file css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 // Esempio preso dal blog -> https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
 
 //Acquisisci elemento HTML dal file che servirà poi per stamparlo
-const lista = document.querySelector('.lista')
+// const lista = document.querySelector('.lista')
 //requiring path and fs modules
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 //Regex to match with
 const regex = /([a-zA-Z0-9\s_\\.\-\(\):])+(.html|.HTML)$/i
-let filesArray = [];
 //joining path of directory 
 const directoryPath = path.join(__dirname, '../newsletters');
 //passing directoryPath and callback function
@@ -20,14 +19,15 @@ fs.readdir(directoryPath, (err, files) => {
     files.forEach(file => {
         // Cerca se i file combaciano con l'espressione regolare e stampali
         if (file.match(regex)) {
-            let nl = document.createElement('div')
-            nl.className ='single'
-            let linkNl = document.createElement('a')
-            linkNl.setAttribute('href',`/newsletters/${file}`)
-            nl.appendChild(linkNl)
-            console.log(linkNl.getAttribute('href'))
-            //stampa nella pagina la singola scheda della newsletter
-            lista.appendChild(nl)
+            // let nl = document.createElement('div')
+            // nl.className ='single'
+            // let linkNl = document.createElement('a')
+            // linkNl.setAttribute('href',`/newsletters/${file}`)
+            // nl.appendChild(linkNl)
+            // console.log(linkNl.getAttribute('href'))
+            // //stampa nella pagina la singola scheda della newsletter
+            // lista.appendChild(nl)
+            console.log(file)
         }
 
     });
