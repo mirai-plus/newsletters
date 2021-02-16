772927d7b85702757bd1834c447b1d3ec52c340a
Francesco Luppi <francescoluppi@aol.com>
2021-02-16 18:28:05 +0100

    FIX: cambiamenti

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,7 @@
 const config = require("./config");
+const fm = require("front-matter");
 const fs = require("fs");
+const marked = require("./marked");
 
 const posthtml = data => `
 <!DOCTYPE html>
@@ -8,9 +10,10 @@ const posthtml = data => `
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${data.attributes.description}" />
-        <link rel="stylesheet" href="../assets/styles/grotesk.light.min.css">
-        <link rel="stylesheet" href="../assets/styles/highlights.min.css">
-        <link rel="stylesheet" href="../assets/styles/main.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css"
+        integrity="sha512-wl80ucxCRpLkfaCnbM88y4AxnutbGk327762eM9E/rRTvY/ZGAHWMZrYUq66VQBYMIYDFpDdJAOGSLyIPHZ2IQ=="
+        crossorigin="anonymous" />
+        <link rel="stylesheet" href="./css/style.css">
         <title>${data.attributes.title}</title>
     </head>
     <body>
@@ -38,8 +41,11 @@ const posthtml = data => `
 `;
 
 const createPost = postPath => {
-  const data = fs.readFileSync(`${config.dev.postsdir}/${postPath}.html`, "utf8");
-  return data;
+  const data = fs.readFileSync(`${config.dev.postsdir}/${postPath}.md`, "utf8");
+  const content = fm(data);
+  content.body = marked(content.body);
+  content.path = postPath;
+  return content;
 };
 
 const createPosts = posts => {
